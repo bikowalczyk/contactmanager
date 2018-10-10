32e6c63629b74dcca704f48dfd9842477ad9f71f
Blazej Kowalczyk <blazej@MacBook-Pro-Blazej.local>
2018-10-10 14:35:47 +0200

    Creating State

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import Contact from './compontents/Contact';
-import Header from './compontents/Header';
+import React, { Component } from "react";
+import Contacts from "./compontents/Contacts";
+import Header from "./compontents/Header";
 
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
-        <Contact name="Smith" email="karen@gmail.com" phone="333-333-3333" />
+        <div className="container">
+          <Contacts />
+        </div>
       </div>
     );
   }
