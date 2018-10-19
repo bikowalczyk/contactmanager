7f3e029c743078cc5ad5025a2bfaee89cd6e5190
Blazej Kowalczyk <blazej@MacBook-Pro-Blazej.local>
2018-10-19 15:11:13 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Contacts from "./compontents/contacts/Contacts";
-import Header from "./compontents/layout/Header";
+
 import AddContact from "./compontents/contacts/AddContact";
+import Header from "./compontents/layout/Header";
+import About from "./compontents/pages/About";
 
 import { Provider } from "./context";
 
@@ -12,13 +15,18 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header branding="Contact Manager" />
-          <div className="container">
-            <AddContact />
-            <Contacts />
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
