959eabe2c67003b7092efb6d4467f3ffd1d8ec37
Blazej Kowalczyk <blazej@MacBook-Pro-Blazej.local>
2018-10-24 22:57:05 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Contacts from './components/contacts/Contacts';
-import AddContact from './components/contacts/AddContact';
-import EditContact from './components/contacts/EditContact';
-import Header from './components/layout/Header';
-import About from './components/pages/About';
-import NotFound from './components/pages/NotFound';
-import Test from './components/test/Test';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Contacts from "./components/contacts/Contacts";
+import AddContact from "./components/contacts/AddContact";
+import EditContact from "./components/contacts/EditContact";
+import Header from "./components/layout/Header";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+import Test from "./components/test/Test";
 
-import { Provider } from './context';
+import { Provider } from "./context";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/boo tstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
